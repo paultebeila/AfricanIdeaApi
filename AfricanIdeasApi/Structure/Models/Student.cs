83de8ed0ae40388d49c18a68b09f8f575e5be47b
Paul Tebeila <paultebeila@gmail.com>
2025-12-08 00:26:40 +0200

    Ignoring some parts from the Json

--- AfricanIdeasApi/Structure/Models/Student.cs
+++ AfricanIdeasApi/Structure/Models/Student.cs
@@ -1,10 +1,14 @@
-﻿namespace AfricanIdeasApi.Structure.Models
+﻿using System.Text.Json.Serialization;
+
+namespace AfricanIdeasApi.Structure.Models
 {
     public class Student
     {
+        [JsonIgnore]
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        [JsonIgnore]
         public List<CourseEnrollments> Enrollments { get; set; } = new();
     }
 }
